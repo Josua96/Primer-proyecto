b5e6e5d26ab01581e53bba7f2b320da4ee0e6905
angelomedina <angelomedinabarraza@gmail.com>
2016-03-17 21:19:12 -0600

    login de estudiantes

---
+++ estudiantes.py
@@ -0,0 +1,24 @@
+Estudiantes=[{"Nombre":"Alberto Ruiz Perez","Correo":"alberto@gmail.com","Contraseña":"0123","Carnet":2016123456},
+              {"Nombre":"Alfredo Castilla Lopez","Correo":"alfredo@gmail.com","Contraseña":"0456","Carnet":2016223456},
+              {"Nombre":"Antonio Umaña Medina","Correo":"anto@gmail.com","Contraseña":"0567","Carnet":2016323456},
+              {"Nombre":"Ariel Rodriguez Jesus","Correo":"ariel@gmail.com","Contraseña":"0678","Carnet":2016423456},
+              {"Nombre":"Benito Acosta Sousa","Correo":"benito@gmail.com","Contraseña":"0789","Carnet":2016523456},
+              {"Nombre":"Berta Bermudez Calderon","Correo":"berta@gmail.com","Contraseña":"0910","Carnet":2016623456},
+              {"Nombre":"Brenda Ferrer Ortiz","Correo":"brenda@gmail.com","Contraseña":"0101","Carnet":2016723456},
+              {"Nombre":"Magaly Rojas Rojas","Correo":"magaly@gmail.com","Contraseña":"0102","Carnet":2016823456},
+              {"Nombre":"Hilary Bustamandte Ureña","Correo":"hilary@gmail.com","Contraseña":"0103","Carnet":2016923456},
+              {"Nombre":"Margara Aagon Romero","Correo":"margar@gmail.com","Contraseña":"0104","Carnet":2016101234},
+              {"Nombre":"Joshua Carranza Perez","Correo":"joshua@gmail.com","Contraseña":"0105","Carnet":2016111234},
+              {"Nombre":"Kervin Sibaja Rodriguez","Correo":"kervin@gmail.com","Contraseña":"0106","Carnet":2016121234},
+              {"Nombre":"Angelo Guerrero Medina","Correo":"angelo@gmail.com","Contraseña":"0107","Carnet":2016131234},
+              {"Nombre":"Andy Herrera Herrera","Correo":"andy@gmail.com","Contraseña":"0108","Carnet":2016141234},
+              {"Nombre":"Carlos Baez Miranza","Correo":"carlos@gmail.com","Contraseña":"0109","Carnet":2016151234},
+              {"Nombre":"Luis Murrillo Barrios","Correo":"luis@gmail.com","Contraseña":"0110","Carnet":2016161234},
+              {"Nombre":"Andrey Carranza Perez","Correo":"andrey@gmail.com","Contraseña":"0111","Carnet":2016171234},
+              {"Nombre":"Ana Gonzalez Gonzalez","Correo":"ana@gmail.com","Contraseña":"0112","Carnet":2016181234},
+              {"Nombre":"Ericka Bello Mendoza","Correo":"ericka@gmail.com","Contraseña":"0113","Carnet":2016191234},
+              {"Nombre":"Rabael Ortega Ortega","Correo":"rafael@gmail.com","Contraseña":"0114","Carnet":2016201234},
+              {"Nombre":"Pedro Pedroza Perez","Correo":"pedro@gmail.com","Contraseña":"0115","Carnet":2016211234}]
+
+
+print(Estudiantes)
